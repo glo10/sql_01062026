0b96d1308a228c8bdb08f088d7b27d10f3442260
glodie <glodie.tshimini@gmail.com>
2026-06-02 14:44:47 +0200

    refactor: add concat with postgreSQL

--- demos/06-select.sql
+++ demos/06-select.sql
@@ -1,2 +1,7 @@
 -- DEMO concaténation
 SELECT CONCAT('cet employé se nomme ', nom, ' ', prenom, ' et occupe la fonction ', fonction) AS 'Liste des employés' FROM employes;
+-- DEMO concaténation avec postgreSQL
+-- Alias avec des guillemets et possible d'utiliser ||
+SELECT CONCAT('cet employé se nomme ', nom, ' ', prenom, ' et occupe la fonction ', fonction) AS "Liste des employés" FROM employes;
+SELECT 'cet employé se nomme ' || nom || ' ' || prenom || ' et occupe la fonction ' || fonction AS "Liste des employés" FROM employes;
+
