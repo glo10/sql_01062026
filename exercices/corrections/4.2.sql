939c3515b1fd86f49944990f3cbeac9c91b9a92d
glodie <glodie.tshimini@gmail.com>
2026-06-02 16:07:08 +0200

    feat: correction 4.2 WHERE

--- exercices/corrections/4.2.sql
+++ exercices/corrections/4.2.sql
@@ -1,12 +1,12 @@
 -- 1. Affichez le nom des clients habitant Marseille
 SELECT societe
 FROM clients
-WHERE ville = 'Marseille';
+WHERE ville='Marseille';
 
 -- 2. Affichez tous les employés dirigés par l'employé ayant l'identifiant numéro 2
 SELECT *
 FROM employes
-WHERE rend_compte = 2;
+WHERE rend_compte=2;
 
 -- 3. Affichez tous les employés qui ne sont pas des représentants
 SELECT *
@@ -16,7 +16,7 @@ WHERE fonction NOT LIKE 'Repr_sentant%';
 -- 4. Affichez tous les employés dont le salaire est en dessous de 3500 €
 SELECT *
 FROM employes
-WHERE salaire < 3500;
+WHERE salaire<3500;
 
 -- 5. Affichez toutes les fonctions qui n'ont pas de supérieur hiérarchique
 SELECT DISTINCT fonction
