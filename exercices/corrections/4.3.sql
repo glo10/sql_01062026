872119b269e816323a8ee3cbda8b82acdea6aeee
glodie <glodie.tshimini@gmail.com>
2026-06-02 16:31:15 +0200

    feat: correction 4.3 ORDER BY

---
+++ exercices/corrections/4.3.sql
@@ -0,0 +1,18 @@
+-- # Exercice 4.3 : lecture des données avec le tri
+
+-- 1. Affichez le nom, prénom des employés triés par le nom de façon décroissante (Z vers A)
+SELECT nom, prenom
+FROM employes
+ORDER BY nom DESC;
+-- 2. Affichez toutes les sociétés et les villes des clients triées par le pays.
+SELECT societe, ville, pays
+FROM clients
+ORDER BY pays;
+-- 3. Affichez les sociétés, pays, villes des clients triés par pays et ville.
+SELECT societe, pays, ville
+FROM clients
+ORDER BY 2, 3;
+-- 4. Affichez le nom et commission des employés triés par commission.
+SELECT nom, commission
+FROM employes
+ORDER BY commission;
