6946d122b4b72d45fe185f7b43860bb88c99347d
glodie <glodie.tshimini@gmail.com>
2026-06-05 10:36:42 +0200

    refactor: replace with postgres

--- demos/16-regex.sql
+++ demos/16-regex.sql
@@ -1,11 +1,21 @@
 -- Supprimer les voyelles
 SELECT REGEXP_REPLACE('Bonjour, c''est Glodie, on est vendredi',
+'[aeiuoy]',
+'');
+-- PostgreSQL
+
+-- Remplacer Glodie par Nathalie
+SELECT REGEXP_REPLACE('Bonjour, c''est Glodie, on est vendredi',
 'Glodie',
 'Nathalie');
 -- Récupérer les produits en boîte ou boîtes ou boite ou boites
 SELECT nom_produit, quantite
 FROM produits
 WHERE REGEXP_LIKE(quantite, 'bo[iî]tes?');
+-- PostgreSQL
+SELECT nom_produit, quantite
+FROM produits
+WHERE quantite ~ 'bo[iî]tes?';
 -- Récupérer la position de la première voyelle dans le nom du client
 SELECT societe, REGEXP_INSTR(societe, '[aeiouy]')
 FROM fournisseurs;
