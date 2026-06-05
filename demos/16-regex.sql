9b116db2d5d5527b22f68a2d8a99ed299b40c38f
glodie <glodie.tshimini@gmail.com>
2026-06-05 10:26:27 +0200

    feat: demo REGEXP

---
+++ demos/16-regex.sql
@@ -0,0 +1,15 @@
+-- Supprimer les voyelles
+SELECT REGEXP_REPLACE('Bonjour, c''est Glodie, on est vendredi',
+'Glodie',
+'Nathalie');
+-- Récupérer les produits en boîte ou boîtes ou boite ou boites
+SELECT nom_produit, quantite
+FROM produits
+WHERE REGEXP_LIKE(quantite, 'bo[iî]tes?');
+-- Récupérer la position de la première voyelle dans le nom du client
+SELECT societe, REGEXP_INSTR(societe, '[aeiouy]')
+FROM fournisseurs;
+-- Compter le nombre caractère dans le nom de la societe
+-- qui ne soit une voyelle
+SELECT societe, REGEXP_COUNT(societe, '[^aeiuoy]')
+FROM fournisseurs;
